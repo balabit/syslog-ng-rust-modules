ae31541fa847712801a7f616e36027913b251f27
Tibor Benke <tibor.benke@balabit.com>
2015-07-29 17:55:49 +0200

    timer: implement Timer
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/timer.rs
+++ src/timer.rs
@@ -1,19 +1,17 @@
 use std::thread;
 use std::sync::mpsc;
-/*
-pub struct Timer {
-    duration: usize,
-    control_channel: mpsc::SyncSender(Option<()>),
-}
+
+pub struct Timer;
 
 impl Timer {
-    pub fn new(duration_ms: usize, out_channel: mpsc::SyncSender<()>) -> Timer {
-        Timer{
-            duration: duration_ms,
-            guard: thread::(|duration| {
-
-            })
-        }
+    pub fn from_chan(ms: u32, tx: mpsc::SyncSender<()>) {
+        thread::spawn(move || {
+            loop {
+                thread::sleep_ms(ms);
+                if tx.send(()).is_err() {
+                    break;
+                }
+            }
+        });
     }
 }
-*/
