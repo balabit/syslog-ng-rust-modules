6b20b1007d17e48890d61fdc7293b6d5fcc21537
Tibor Benke <tibor.benke@balabit.com>
2015-08-07 08:57:07 +0200

    Correlator: rename tx to dispatcher_input_channel
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/correlator.rs
+++ src/correlator.rs
@@ -7,14 +7,14 @@ use super::{config, Command, Context, Dispatcher, Event, Message, Timer};
 const TIMER_STEP: u32 = 100;
 
 pub struct Correlator {
-    tx: mpsc::Sender<Command>,
+    dispatcher_input_channel: mpsc::Sender<Command>,
     dispatcher_thread_handle: thread::JoinHandle<()>
 }
 
 impl Correlator {
     pub fn new(contexts: Vec<config::Context>) -> Correlator {
-        let (tx, rx) = mpsc::channel();
-        let _ = Timer::from_chan(TIMER_STEP, tx.clone());
+        let (dispatcher_input_channel, rx) = mpsc::channel();
+        let _ = Timer::from_chan(TIMER_STEP, dispatcher_input_channel.clone());
 
         let handle = thread::spawn(move || {
             let mut dispatcher = Dispatcher::new(contexts);
@@ -28,17 +28,17 @@ impl Correlator {
         });
 
         Correlator {
-            tx: tx,
+            dispatcher_input_channel: dispatcher_input_channel,
             dispatcher_thread_handle: handle
         }
     }
 
     pub fn push_message(&mut self, message: Message) -> Result<(), mpsc::SendError<Command>> {
-        self.tx.send(Command::Dispatch(Event::Message(message)))
+        self.dispatcher_input_channel.send(Command::Dispatch(Event::Message(message)))
     }
 
     pub fn stop(self) -> thread::Result<()> {
-        let _ = self.tx.send(Command::Exit);
+        let _ = self.dispatcher_input_channel.send(Command::Exit);
         self.dispatcher_thread_handle.join()
     }
 }
