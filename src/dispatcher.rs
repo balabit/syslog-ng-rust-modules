d13082fec02548675302cbc75faecfd0ca9dfa8d
Tibor Benke <tibor.benke@balabit.com>
2015-08-07 16:25:10 +0200

    dispatcher: forward commands to the outside
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -2,7 +2,7 @@ use std::sync::mpsc::Sender;
 use std::rc::Rc;
 
 use action::ActionCommand;
-use super::{config, Context, Event};
+use super::{config, Context, Event, Message, TimerEvent};
 
 pub struct Dispatcher {
     contexts: Vec<Context>,
@@ -19,16 +19,37 @@ impl Dispatcher {
     }
 
     pub fn dispatch(&mut self, event: Event) {
+        let mut commands = Vec::new();
         match event {
             Event::Message(event) => {
                 let event = Rc::new(event);
-                for context in self.contexts.iter_mut() {
-                    context.on_message(event.clone());
-                }
+                self.on_message(&mut commands, event);
             },
             Event::Timer(ref event) => {
-                for context in self.contexts.iter_mut() {
-                    context.on_timer(event);
+                self.on_timer(&mut commands, event);
+            }
+        };
+
+        for i in commands.into_iter() {
+            self.output_channel.send(i);
+        }
+    }
+
+    fn on_message(&mut self, commands: &mut Vec<ActionCommand>, event: Rc<Message>) {
+        for context in self.contexts.iter_mut() {
+            if let Some(result) = context.on_message(event.clone()) {
+                for i in result.into_iter() {
+                    commands.push(i);
+                }
+            }
+        }
+    }
+
+    fn on_timer(&mut self, commands: &mut Vec<ActionCommand>, event: &TimerEvent) {
+        for context in self.contexts.iter_mut() {
+            if let Some(result) = context.on_timer(event) {
+                for i in result.into_iter() {
+                    commands.push(i);
                 }
             }
         }
