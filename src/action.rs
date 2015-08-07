532ec8215ff787a5c8642554a31ae59117c93dbe
Tibor Benke <tibor.benke@balabit.com>
2015-08-07 10:15:24 +0200

    action: add ActionCommand enum
    It represents the commands which should be executed in the parser thread.
    
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/action.rs
+++ src/action.rs
@@ -2,6 +2,7 @@ use state::State;
 use context::Context;
 
 use self::message::Action as MessageAction;
+use self::message::Command as MessageCommand;
 
 #[derive(Debug)]
 pub enum Action {
@@ -9,24 +10,31 @@ pub enum Action {
 }
 
 impl Action {
-    pub fn execute(&self, state: &State, context: &Context) {
+    pub fn execute(&self, state: &State, context: &Context) -> ActionCommand {
         match *self {
             Action::Message(ref action) => action.execute(state, context)
         }
     }
 }
 
+pub enum ActionCommand {
+    Message(MessageCommand)
+}
+
 mod message {
     use context::Context;
     use state::State;
     use Message;
+    use super::ActionCommand;
 
     #[derive(Debug)]
     pub struct Action;
 
     impl Action {
-        pub fn execute(&self, state: &State, context: &Context) {
-            println!("Message action executed");
+        pub fn execute(&self, state: &State, context: &Context) -> ActionCommand {
+            ActionCommand::Message(Command(Message::new("".to_string())))
         }
     }
+
+    pub struct Command(Message);
 }
