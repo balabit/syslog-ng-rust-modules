ebc801a4b575d9537d49bba88da8f2959782383f
Tibor Benke <tibor.benke@balabit.com>
2015-08-08 15:02:11 +0200

    test: update test program
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -10,21 +10,30 @@ use std::thread;
 struct Printer;
 
 impl message::ActionHandler for Printer {
-    fn handle(&mut self, command: message::ExecResult) {
-        println!("{:?}", &command);
+    fn handle(&mut self, _: message::ExecResult) {
+        println!("I'm the message handler");
     }
 }
 
+#[allow(dead_code)]
 fn main() {
+    let patterns = vec!["1".to_string(), "2".to_string(), "3".to_string()];
+    let condition = conditions::Builder::new(100).patterns(patterns)
+                                                .first_opens(true)
+                                                .last_closes(true)
+                                                .build();
+    let actions = vec![ message::Action::new().into() ];
     let contexts = vec!{
-        config::Context::new(conditions::Builder::new(100).build()),
-        config::Context::new(conditions::Builder::new(100).build()),
-        config::Context::new(conditions::Builder::new(100).build()),
+        config::ContextBuilder::new(condition.clone()).actions(actions.clone()).build(),
+        config::ContextBuilder::new(condition.clone()).actions(actions.clone()).build(),
+        config::ContextBuilder::new(condition.clone()).actions(actions.clone()).build(),
     };
     let handlers = ActionHandlers::new(Box::new(Printer));
     let mut correlator = Correlator::new(contexts, handlers);
-    let msg1 = Message::new("1".to_string());
-    let _ = correlator.push_message(msg1);
-    thread::sleep_ms(2000);
+    let _ = correlator.push_message(Message::new("1".to_string()));
+    thread::sleep_ms(20);
+    let _ = correlator.push_message(Message::new("2".to_string()));
+    thread::sleep_ms(80);
+    let _ = correlator.push_message(Message::new("3".to_string()));
     let _ = correlator.stop();
 }
