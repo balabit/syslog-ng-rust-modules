a2dd8e4654674c58a34ffecd19dc97bbaaba8650
Tibor Benke <ihrwein@gmail.com>
2015-08-08 18:56:07 +0200

    Correlator: refactor stop() method
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/correlator.rs
+++ src/correlator.rs
@@ -2,7 +2,6 @@ use std::sync::mpsc;
 use std::thread;
 use std::result::Result;
 
-use action::ExecResult;
 use action::ActionHandlers;
 use super::{config, Command, CommandResult, Context, Dispatcher, Event, Message, Timer};
 
@@ -36,38 +35,44 @@ impl Correlator {
     }
 
     pub fn push_message(&mut self, message: Message) -> Result<(), mpsc::SendError<Command>> {
-        Correlator::consume_results(&mut self.dispatcher_output_channel, &mut self.action_handlers);
+        self.consume_results();
         self.dispatcher_input_channel.send(Command::Dispatch(Event::Message(message)))
     }
 
-    fn consume_results(channel: &mut mpsc::Receiver<CommandResult>, handlers: &mut ActionHandlers) {
-        for i in channel.try_recv() {
+    fn consume_results(&mut self) {
+        for i in self.dispatcher_output_channel.try_recv() {
             if let CommandResult::Dispatch(result) = i {
-                handlers.handle(result);
+                self.action_handlers.handle(result);
             }
         }
     }
 
-    fn consume_all_remaining_results(channel: &mut mpsc::Receiver<CommandResult>, handlers: &mut ActionHandlers) {
-        for i in channel.recv() {
-            if let CommandResult::Dispatch(result) = i {
-                handlers.handle(result);
+    fn stop_dispatcher(&mut self) {
+        let _ = self.dispatcher_input_channel.send(Command::Exit);
+        let mut exit_num = 0;
+        loop {
+            let value = self.dispatcher_output_channel.recv();
+            match value {
+                Ok(value) => {
+                    match value {
+                        CommandResult::Dispatch(result) => self.action_handlers.handle(result),
+                        CommandResult::Exit => {
+                            let _ = self.dispatcher_input_channel.send(Command::Exit);
+                            exit_num += 1;
+                            if exit_num >= 1 {
+                                break;
+                            }
+                        }
+                    }
+                },
+                Err(_) => {}
             }
         }
     }
 
-    pub fn stop(self) -> thread::Result<()> {
-        let Correlator {
-            action_handlers: mut handlers,
-            dispatcher_input_channel: input,
-            dispatcher_output_channel: mut output,
-            dispatcher_thread_handle: thread_handle } = self;
-
-        Correlator::consume_results(&mut output, &mut handlers);
-        let _ = input.send(Command::Exit);
-        Correlator::consume_results(&mut output, &mut handlers);
-        let join_result = thread_handle.join();
-        Correlator::consume_all_remaining_results(&mut output, &mut handlers);
-        join_result
+    pub fn stop(mut self) -> thread::Result<()> {
+        self.consume_results();
+        self.stop_dispatcher();
+        self.dispatcher_thread_handle.join()
     }
 }
