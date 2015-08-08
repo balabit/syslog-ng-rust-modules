18064634f8d85dc3ce2f3b5e085953c26a0bbb54
Tibor Benke <tibor.benke@balabit.com>
2015-08-08 09:50:18 +0200

    Correlator: use ActionHandlers
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -3,7 +3,17 @@ extern crate maplit;
 extern crate correlation;
 
 use correlation::{config, conditions, Correlator, Message};
+use correlation::action::message::{MessageActionHandler, Command};
+use correlation::action::ActionHandlers;
 use std::thread;
+
+struct Printer;
+
+impl MessageActionHandler for Printer {
+    fn handle(&mut self, command: Command) {
+        println!("{:?}", &command);
+    }
+}
 
 fn main() {
     let contexts = vec!{
@@ -11,7 +21,8 @@ fn main() {
         config::Context::new(conditions::Builder::new(100).build()),
         config::Context::new(conditions::Builder::new(100).build()),
     };
-    let mut correlator = Correlator::new(contexts);
+    let handlers = ActionHandlers::new(Box::new(Printer));
+    let mut correlator = Correlator::new(contexts, handlers);
     let msg1 = Message::new("1".to_string());
     let _ = correlator.push_message(msg1);
     thread::sleep_ms(2000);
--- src/correlator.rs
+++ src/correlator.rs
@@ -2,17 +2,21 @@ use std::sync::mpsc;
 use std::thread;
 use std::result::Result;
 
+use action::ActionCommand;
+use action::ActionHandlers;
 use super::{config, Command, Context, Dispatcher, Event, Message, Timer};
 
 const TIMER_STEP: u32 = 100;
 
 pub struct Correlator {
+    action_handlers: ActionHandlers,
     dispatcher_input_channel: mpsc::Sender<Command>,
+    dispatcher_output_channel: mpsc::Receiver<ActionCommand>,
     dispatcher_thread_handle: thread::JoinHandle<()>
 }
 
 impl Correlator {
-    pub fn new(contexts: Vec<config::Context>) -> Correlator {
+    pub fn new(contexts: Vec<config::Context>, action_handlers: ActionHandlers) -> Correlator {
         let (dispatcher_input_channel, rx) = mpsc::channel();
         let (dispatcher_output_channel_tx, dispatcher_output_channel_rx) = mpsc::channel();
         let _ = Timer::from_chan(TIMER_STEP, dispatcher_input_channel.clone());
@@ -29,12 +33,18 @@ impl Correlator {
         });
 
         Correlator {
+            action_handlers: action_handlers,
             dispatcher_input_channel: dispatcher_input_channel,
+            dispatcher_output_channel: dispatcher_output_channel_rx,
             dispatcher_thread_handle: handle
         }
     }
 
     pub fn push_message(&mut self, message: Message) -> Result<(), mpsc::SendError<Command>> {
+        for i in self.dispatcher_output_channel.try_recv() {
+            self.action_handlers.handle(i);
+        }
+
         self.dispatcher_input_channel.send(Command::Dispatch(Event::Message(message)))
     }
 
