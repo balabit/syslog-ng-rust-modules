5c703c8aa88abd900223a397c8274ffa338b8ded
Tibor Benke <tibor.benke@balabit.com>
2015-08-13 17:05:28 +0200

    lib: implement Evet for Event
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/lib.rs
+++ src/lib.rs
@@ -33,3 +33,19 @@ pub enum Event {
     Timer(TimerEvent),
     Message(Message)
 }
+
+#[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+pub enum EventHandler {
+    Timer,
+    Message
+}
+
+impl reactor::Event for Event {
+    type Handler = EventHandler;
+    fn handler(&self) -> Self::Handler {
+        match *self {
+            Event::Message(_) => EventHandler::Message,
+            Event::Timer(_) => EventHandler::Timer
+        }
+    }
+}
