7ffbe740775b75809438166781383599d21cf713
Tibor Benke <tibor.benke@balabit.com>
2015-08-13 15:23:32 +0200

    reactor: bring back the Handler types
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/reactor.rs
+++ src/reactor.rs
@@ -1,5 +1,7 @@
 pub trait EventHandler<T> {
+    type Handler;
     fn handle_event(&mut self, event: T);
+    fn handler(&self) -> Self::Handler;
 }
 
 pub trait EventDemultiplexer {
@@ -8,8 +10,14 @@ pub trait EventDemultiplexer {
 }
 
 pub trait Reactor {
-    type Event;
+    type Event: Event;
+    type Handler;
     fn handle_events(&mut self);
-    fn register_handler(&mut self, handler: Box<EventHandler<Self::Event>>);
-    fn remove_handler(&mut self, handler: &EventHandler<Self::Event>);
+    fn register_handler(&mut self, handler: Box<EventHandler<Self::Event, Handler=Self::Handler>>);
+    fn remove_handler(&mut self, handler: &EventHandler<Self::Event, Handler=Self::Handler>);
+}
+
+pub trait Event {
+    type Handler;
+    fn handler(&self) -> Self::Handler;
 }
