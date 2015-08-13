36c65113dc4afd1f31d8cce378212e6e2d080ccd
Tibor Benke <tibor.benke@balabit.com>
2015-08-13 13:14:25 +0200

    lib: move condition module into a top level module
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

---
+++ src/condition.rs
@@ -0,0 +1,19 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Clone, Debug)]
+pub struct Condition(Rc<RefCell<bool>>);
+
+impl Condition {
+    pub fn is_active(&self) -> bool {
+        *self.0.borrow()
+    }
+
+    pub fn activate(&mut self) {
+        *self.0.borrow_mut() = true;
+    }
+
+    pub fn deactivate(&mut self) {
+        *self.0.borrow_mut() = false;
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ extern crate maplit;
 mod macros;
 
 pub use action::Action;
+pub use condition::Condition;
 pub use conditions::Conditions;
 pub use context::Context;
 pub use correlator::Correlator;
@@ -16,6 +17,7 @@ pub use timer::{Timer,
 pub mod conditions;
 pub mod config;
 pub mod action;
+mod condition;
 mod context;
 mod correlator;
 mod dispatcher;
