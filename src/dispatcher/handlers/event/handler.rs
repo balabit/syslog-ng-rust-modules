8cf27f1b98129f54c5ac123c8dcbc00665abdcac
Tibor Benke <ihrwein@gmail.com>
2015-08-15 19:58:42 +0200

    dispatcher::handlers::event: export Handler
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

---
+++ src/dispatcher/handlers/event/handler.rs
@@ -0,0 +1,45 @@
+use std::collections::BTreeMap;
+
+use dispatcher::request::{Request, RequestHandler};
+use reactor::{self, EventHandler, Event};
+
+use dispatcher::handlers::event::timer::TimerEventHandler;
+use dispatcher::handlers::event::message::MessageHandler;
+
+pub struct Handler{
+    handlers: BTreeMap<::EventHandler, Box<reactor::EventHandler<::Event, Handler=::EventHandler>>>,
+}
+
+impl Handler {
+    pub fn new() -> Handler {
+        let timer_handler = Box::new(TimerEventHandler::new());
+        let message_handler = Box::new(MessageHandler::new());
+        let mut handler = Handler{
+            handlers: BTreeMap::new()
+        };
+        handler.register_handler(timer_handler);
+        handler.register_handler(message_handler);
+        handler
+    }
+
+    fn register_handler(&mut self, handler: Box<reactor::EventHandler<::Event, Handler=::EventHandler>>) {
+        self.handlers.insert(handler.handler(), handler);
+    }
+}
+
+impl reactor::EventHandler<Request> for Handler {
+    type Handler = RequestHandler;
+    fn handle_event(&mut self, event: Request) {
+        if let Request::Event(event) = event {
+            println!("Event recvd");
+            if let Some(handler) = self.handlers.get_mut(&event.handler()) {
+                handler.handle_event(event);
+            }
+        } else {
+            unreachable!("An Handler should only receive Event events");
+        }
+    }
+    fn handler(&self) -> Self::Handler {
+        RequestHandler::Event
+    }
+}
--- src/dispatcher/handlers/event/mod.rs
+++ src/dispatcher/handlers/event/mod.rs
@@ -1,48 +1,5 @@
-use std::collections::BTreeMap;
+pub use self::handler::Handler;
 
-use dispatcher::request::{Request, RequestHandler};
-use reactor::{self, EventHandler, Event};
-
-use self::timer::TimerEventHandler;
-use self::message::MessageHandler;
-
-pub struct Handler{
-    handlers: BTreeMap<::EventHandler, Box<reactor::EventHandler<::Event, Handler=::EventHandler>>>,
-}
-
-impl Handler {
-    pub fn new() -> Handler {
-        let timer_handler = Box::new(TimerEventHandler::new());
-        let message_handler = Box::new(MessageHandler::new());
-        let mut handler = Handler{
-            handlers: BTreeMap::new()
-        };
-        handler.register_handler(timer_handler);
-        handler.register_handler(message_handler);
-        handler
-    }
-
-    fn register_handler(&mut self, handler: Box<reactor::EventHandler<::Event, Handler=::EventHandler>>) {
-        self.handlers.insert(handler.handler(), handler);
-    }
-}
-
-impl reactor::EventHandler<Request> for Handler {
-    type Handler = RequestHandler;
-    fn handle_event(&mut self, event: Request) {
-        if let Request::Event(event) = event {
-            println!("Event recvd");
-            if let Some(handler) = self.handlers.get_mut(&event.handler()) {
-                handler.handle_event(event);
-            }
-        } else {
-            unreachable!("An Handler should only receive Event events");
-        }
-    }
-    fn handler(&self) -> Self::Handler {
-        RequestHandler::Event
-    }
-}
-
+pub mod handler;
 pub mod message;
 pub mod timer;
