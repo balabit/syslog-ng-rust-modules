8c8e85a36d013d2c88e057f98842ec99848b2e66
Tibor Benke <ihrwein@gmail.com>
2015-08-15 19:48:20 +0200

    dispatcher::handlers::exit: move it into its own module
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/dispatcher/handlers/mod.rs
+++ src/dispatcher/handlers/mod.rs
@@ -1,40 +1,4 @@
-pub mod exit {
-    use dispatcher::request::{Request, RequestHandler};
-    use condition::Condition;
-    use reactor::EventHandler;
-
-    pub struct ExitHandler{
-        condition: Condition,
-        stops: u32
-    }
-
-    impl ExitHandler {
-        pub fn new(condition: Condition) -> ExitHandler {
-            ExitHandler {
-                condition: condition,
-                stops: 0
-            }
-        }
-    }
-
-    impl EventHandler<Request> for ExitHandler {
-        type Handler = RequestHandler;
-        fn handle_event(&mut self, event: Request) {
-            if let Request::Exit = event {
-                self.stops += 1;
-
-                if self.stops >= 2 {
-                    self.condition.activate();
-                }
-            } else {
-                unreachable!("An ExitHandler should only receive Exit events");
-            }
-        }
-        fn handler(&self) -> Self::Handler {
-            RequestHandler::Exit
-        }
-    }
-}
+pub mod exit;
 
 pub mod event {
     use std::collections::BTreeMap;
