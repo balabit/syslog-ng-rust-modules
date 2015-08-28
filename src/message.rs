3df8d1cb6d70754f70ab985da13e0a0dab3291fe
Tibor Benke <tibor.benke@balabit.com>
2015-08-28 15:37:45 +0200

    PatternId: implement serde::de::Deserialize trait
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/message.rs
+++ src/message.rs
@@ -60,3 +60,69 @@ pub enum PatternId {
     Uuid(String),
     Name(String)
 }
+
+mod deser {
+    use super::PatternId;
+    use serde;
+    use serde::de::Deserialize;
+
+    const VARIANTS: &'static [&'static str] = &["uuid", "name"];
+
+    impl serde::Deserialize for PatternId {
+        fn deserialize<D>(deserializer: &mut D) -> Result<PatternId, D::Error>
+            where D: serde::de::Deserializer
+        {
+            deserializer.visit_enum("PatternId", VARIANTS, PatternIdVisitor)
+        }
+    }
+
+    enum Field {
+        Uuid,
+        Name,
+    }
+
+    impl serde::Deserialize for Field {
+        fn deserialize<D>(deserializer: &mut D) -> Result<Field, D::Error>
+            where D: serde::de::Deserializer
+        {
+            struct FieldVisitor;
+
+            impl serde::de::Visitor for FieldVisitor {
+                type Value = Field;
+
+                fn visit_str<E>(&mut self, value: &str) -> Result<Field, E>
+                    where E: serde::de::Error
+                {
+                    match value {
+                        "uuid" => Ok(Field::Uuid),
+                        "name" => Ok(Field::Name),
+                        name @ _ => Err(serde::de::Error::unknown_field(name)),
+                    }
+                }
+            }
+
+            deserializer.visit(FieldVisitor)
+        }
+    }
+
+    struct PatternIdVisitor;
+
+    impl serde::de::EnumVisitor for PatternIdVisitor {
+        type Value = PatternId;
+
+        fn visit<V>(&mut self, mut visitor: V) -> Result<PatternId, V::Error>
+            where V: serde::de::VariantVisitor
+        {
+            match try!(visitor.visit_variant()) {
+                Field::Uuid => {
+                    let value = try!(visitor.visit_newtype());
+                    Ok(PatternId::Uuid(value))
+                },
+                Field::Name => {
+                    let value = try!(visitor.visit_newtype());
+                    Ok(PatternId::Name(value))
+                },
+            }
+        }
+    }
+}
