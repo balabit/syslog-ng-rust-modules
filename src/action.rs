ac4dda14f809c19c90205ebae6ebaa93b2178d48
Tibor Benke <ihrwein@gmail.com>
2015-08-29 15:45:39 +0200

    MessageAction: implement serde::Deserialize trait for MessageAction
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/action.rs
+++ src/action.rs
@@ -82,4 +82,29 @@ pub mod message {
     pub trait ActionHandler {
         fn handle(&mut self, command: ExecResult);
     }
+
+    mod deser {
+
+    use super::MessageAction;
+    use serde;
+    use serde::de::Deserialize;
+
+    impl serde::Deserialize for MessageAction {
+        fn deserialize<D>(deserializer: &mut D) -> Result<MessageAction, D::Error>
+            where D: serde::de::Deserializer
+        {
+            deserializer.visit_unit_struct("MessageAction", MessageActionVisitor)
+        }
+    }
+
+    struct MessageActionVisitor;
+
+    impl serde::de::Visitor for MessageActionVisitor {
+        type Value = MessageAction;
+        fn visit_unit<E>(&mut self) -> Result<Self::Value, E>
+            where E: serde::de::Error {
+            Ok(MessageAction)
+        }
+    }
+    }
 }
