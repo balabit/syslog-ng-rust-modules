cb828de5b85410d9391e5c49d42894a20bfd8325
Tibor Benke <ihrwein@gmail.com>
2015-09-05 13:46:05 +0200

    actions: split into smaller modules
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

---
+++ src/action/message.rs
@@ -0,0 +1,39 @@
+use action::Action;
+use config;
+use context::base::BaseContext;
+use dispatcher::Response;
+use dispatcher::response::ResponseSender;
+use message::{Builder, Message};
+use std::cell::RefCell;
+use std::rc::Rc;
+use state::State;
+
+#[derive(Clone)]
+pub struct MessageAction {
+    pub sender: Rc<RefCell<Box<ResponseSender<Response>>>>,
+    pub action: config::action::MessageAction
+}
+
+#[derive(Debug)]
+pub struct MessageResponse {
+    message: Message,
+    internal: bool
+}
+
+impl Action for MessageAction {
+    fn execute(&self, _state: &State, _context: &BaseContext) {
+        println!("MessageAction is executed");
+        let mut message = Builder::new("d6621bd6-4898-4b8c-a4ff-36d0eed7d8dc")
+                                .pair(".context.uuid", &_context.uuid().to_hyphenated_string())
+                                .pair(".context.len", &_state.messages().len().to_string())
+                                .build();
+        if let Some(name) = _context.name() {
+            message.insert(".context.name", name);
+        }
+        let response = MessageResponse {
+            message: message,
+            internal: true
+        };
+        self.sender.borrow_mut().send_response(Response::Message(response));
+    }
+}
