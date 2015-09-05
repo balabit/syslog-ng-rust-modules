5ef83c1f8e6bad7a4eceeceb3175f5e888198aa6
Tibor Benke <ihrwein@gmail.com>
2015-09-05 12:53:29 +0200

    config::action::MessageAction: split into smaller modules
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/config/action/message/mod.rs
+++ src/config/action/message/mod.rs
@@ -1,5 +1,7 @@
 use super::ActionType;
 
+mod deser;
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct MessageAction;
 
@@ -13,29 +15,4 @@ impl From<MessageAction> for super::ActionType {
     fn from(action: MessageAction) -> super::ActionType {
         super::ActionType::Message(action)
     }
-}
-
-mod deser {
-
-use super::MessageAction;
-use serde;
-use serde::de::Deserialize;
-
-impl serde::Deserialize for MessageAction {
-    fn deserialize<D>(deserializer: &mut D) -> Result<MessageAction, D::Error>
-        where D: serde::de::Deserializer
-    {
-        deserializer.visit_unit_struct("MessageAction", MessageActionVisitor)
-    }
-}
-
-struct MessageActionVisitor;
-
-impl serde::de::Visitor for MessageActionVisitor {
-    type Value = MessageAction;
-    fn visit_unit<E>(&mut self) -> Result<Self::Value, E>
-        where E: serde::de::Error {
-        Ok(MessageAction)
-    }
-}
 }
