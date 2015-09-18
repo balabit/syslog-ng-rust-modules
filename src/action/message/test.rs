16093d3b90247445a57d06e116eb0581e1dc6670
Tibor Benke <tibor.benke@balabit.com>
2015-09-18 18:08:02 +0200

    tests: add a message template rendering test
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/action/message/test.rs
+++ src/action/message/test.rs
@@ -1,5 +1,6 @@
 use context::base::BaseContextBuilder;
 use super::{
+    CONTEXT_LEN,
     CONTEXT_NAME,
     CONTEXT_UUID,
     MessageAction,
@@ -10,8 +11,10 @@ use conditions::ConditionsBuilder;
 use config;
 use dispatcher::Response;
 use dispatcher::response::ResponseSender;
+use message::MessageBuilder;
 use state::State;
 
+use env_logger;
 use handlebars::Template;
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -53,6 +56,49 @@ fn test_given_a_message_action_when_it_is_executed_then_it_adds_the_name_and_uui
     if let &Response::Message(ref response) = responses.get(0).unwrap() {
         assert_eq!(name.as_ref().unwrap(), response.message().get(CONTEXT_NAME).unwrap());
         assert_eq!(&base_context.uuid().to_hyphenated_string(), response.message().get(CONTEXT_UUID).unwrap());
+        assert_eq!("0", response.message().get(CONTEXT_LEN).unwrap());
+    } else {
+        unreachable!();
+    }
+}
+
+#[test]
+fn test_given_message_action_when_it_is_executed_then_it_uses_the_messages_to_render_the_message_and_additonal_templated_values() {
+    let _ = env_logger::init();
+    let name = Some("name".to_string());
+    let base_context = {
+        let conditions = ConditionsBuilder::new(100).build();
+        let uuid = Uuid::new_v4();
+        BaseContextBuilder::new(uuid, conditions).name(name.clone()).build()
+    };
+    let state = {
+        let mut state = State::new();
+        state.add_message(Rc::new(MessageBuilder::new("uuid1", "message1").pair("key1", "value1").build()));
+        state.add_message(Rc::new(MessageBuilder::new("uuid2", "message2").pair("key2", "value2").build()));
+        state
+    };
+    let responses = Rc::new(RefCell::new(Vec::new()));
+    let message_action = {
+        let response_sender = DummyResponseSender {responses: responses.clone()};
+        let message = Template::compile("key1={{{messages.[0].values.key1}}} key2={{{messages.[1].values.key2}}}".to_string()).ok().expect("Failed to compile a handlebars template");
+        let config_action = config::action::message::MessageActionBuilder::new("uuid", message)
+                                .pair("message_num", Template::compile("we have {{context_len}} messages".to_string()).ok().expect("Failed to compile a handlebars template"))
+                                .build();
+        MessageAction::new(
+            Rc::new(RefCell::new(Box::new(response_sender))),
+            config_action
+        )
+    };
+
+    message_action.execute(&state, &base_context);
+    assert_eq!(1, responses.borrow().len());
+    let responses = responses.borrow();
+    if let &Response::Message(ref response) = responses.get(0).unwrap() {
+        assert_eq!(name.as_ref().unwrap(), response.message().get(CONTEXT_NAME).unwrap());
+        assert_eq!(&base_context.uuid().to_hyphenated_string(), response.message().get(CONTEXT_UUID).unwrap());
+        let message = response.message();
+        assert_eq!("we have 2 messages", message.get("message_num").expect("Failed to get an inserted key from a map"));
+        assert_eq!("key1=value1 key2=value2", message.message());
     } else {
         unreachable!();
     }
