1f007098b5102ba563e51b5394c7f1a9f9241d77
Tibor Benke <ihrwein@gmail.com>
2015-10-23 16:35:14 +0200

    sys: add LogParser type
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- syslog-ng-sys/src/lib.rs
+++ syslog-ng-sys/src/lib.rs
@@ -7,9 +7,11 @@ pub mod logmsg;
 pub mod ffi;
 mod cfg;
 pub mod messages;
+pub mod logparser;
 
 pub use types::*;
 pub use logmsg::*;
 pub use ffi::*;
 pub use cfg::*;
 pub use messages::*;
+pub use logparser::LogParser;
