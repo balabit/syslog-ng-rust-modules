91b2b9c5cfaa3e1706d0f66c50749c40921ff3b6
Tibor Benke <ihrwein@gmail.com>
2015-10-24 16:07:17 +0200

    syslog-ng-common: move RustParserProxy into its own module
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- syslog-ng-common/src/proxies/parser/mod.rs
+++ syslog-ng-common/src/proxies/parser/mod.rs
@@ -2,55 +2,10 @@ use syslog_ng_sys::LogMessage;
 use syslog_ng_sys::LogParser;
 
 mod option_error;
+mod proxy;
+
 pub use self::option_error::OptionError;
-
-#[repr(C)]
-#[derive(Clone)]
-pub struct RustParserProxy<P> where P: RustParser {
-    pub parser: Option<P>,
-    pub builder: Option<P::Builder>
-}
-
-impl<P> RustParserProxy<P> where P: RustParser {
-    pub fn new() -> RustParserProxy<P> {
-        RustParserProxy {
-            parser: None,
-            builder: Some(P::Builder::new())
-        }
-    }
-
-    pub fn init(&mut self) -> bool {
-        let builder = self.builder.take().expect("Called init when builder was not set");
-        match builder.build() {
-            Ok(parser) => {
-                self.parser = Some(parser);
-                self.parser.as_mut().expect("Called init on a non-existing Rust Parser").init()
-            },
-            Err(error) => {
-                error!("Error: {:?}", error);
-                false
-            }
-        }
-    }
-
-    pub fn set_option(&mut self, name: String, value: String) {
-        if self.builder.is_none() {
-            self.builder = Some(P::Builder::new());
-        }
-
-        let builder = self.builder.as_mut().expect("Failed to get builder on a RustParserProxy");
-        builder.option(name, value);
-    }
-
-    pub fn process(&mut self, msg: &mut LogMessage, input: &str) -> bool {
-        self.parser.as_mut().expect("Called process on a non-existing Rust parser").process(msg, input)
-    }
-
-    pub fn parent(&mut self, parent: *mut LogParser) {
-        let builder = self.builder.as_mut().expect("Failed to get a builder on a new parser proxy instance");
-        builder.parent(parent);
-    }
-}
+pub use self::proxy::RustParserProxy;
 
 pub trait RustParserBuilder: Clone {
     type Parser: RustParser;
