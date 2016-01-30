69f31d835a2388775358d1ddd8374ff2a6bddbed
Tibor Benke <ihrwein@gmail.com>
2016-01-30 13:19:14 +0100

    Condition: use Cell instead of RefCell
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/condition.rs
+++ src/condition.rs
@@ -1,23 +1,23 @@
-use std::cell::RefCell;
+use std::cell::Cell;
 use std::rc::Rc;
 
 #[derive(Clone, Debug)]
-pub struct Condition(Rc<RefCell<bool>>);
+pub struct Condition(Rc<Cell<bool>>);
 
 impl Condition {
     pub fn new(state: bool) -> Condition {
-        Condition(Rc::new(RefCell::new(state)))
+        Condition(Rc::new(Cell::new(state)))
     }
 
     pub fn is_active(&self) -> bool {
-        *self.0.borrow()
+        self.0.get()
     }
 
     pub fn activate(&mut self) {
-        *self.0.borrow_mut() = true;
+        self.0.set(true);
     }
 
     pub fn deactivate(&mut self) {
-        *self.0.borrow_mut() = false;
+        self.0.set(false);
     }
 }
