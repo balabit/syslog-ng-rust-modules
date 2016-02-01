3fd27ff67df3d66e366f8facad70d9f66a35979d
Tibor Benke <ihrwein@gmail.com>
2016-02-01 18:51:59 +0100

    ResponseSender: impl ResponseSender for Sender<Response>
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/dispatcher/response.rs
+++ src/dispatcher/response.rs
@@ -1,6 +1,17 @@
+use std::sync::mpsc::Sender;
+
 use super::Response;
 
 pub trait ResponseSender {
     fn send_response(&self, response: Response);
     fn boxed_clone(&self) -> Box<ResponseSender>;
+}
+
+impl ResponseSender for Sender<Response> {
+    fn send_response(&self, response: Response) {
+        let _ = self.send(response);
+    }
+    fn boxed_clone(&self) -> Box<ResponseSender> {
+        Box::new(self.clone())
+    }
 }
