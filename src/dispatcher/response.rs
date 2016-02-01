8778d6ac3aabcd28d838a65b6712281512fe1a1b
Tibor Benke <ihrwein@gmail.com>
2016-02-01 20:03:47 +0100

    dispatcher/response: add MockResponseSender
    It just collect the responses into a vector. Useful for testing purposes.
    
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/dispatcher/response.rs
+++ src/dispatcher/response.rs
@@ -15,3 +15,21 @@ impl ResponseSender for Sender<Response> {
         Box::new(self.clone())
     }
 }
+
+#[derive(Clone)]
+pub struct MockResponseSender(pub Vec<Response>);
+
+impl MockResponseSender {
+    pub fn new() -> MockResponseSender {
+        MockResponseSender(Vec::new())
+    }
+}
+
+impl ResponseSender for MockResponseSender {
+    fn send_response(&mut self, response: Response) {
+        self.0.push(response);
+    }
+    fn boxed_clone(&self) -> Box<ResponseSender> {
+        Box::new(self.clone())
+    }
+}
