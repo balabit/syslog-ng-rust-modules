1f6411719c6fb7d173be3de376d9d4de6dcbc03b
Tibor Benke <ihrwein@gmail.com>
2016-02-01 18:59:49 +0100

    RequestReactor: use higher-ranked lifetime
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/dispatcher/reactor.rs
+++ src/dispatcher/reactor.rs
@@ -3,11 +3,11 @@ use std::collections::BTreeMap;
 use context::ContextMap;
 use dispatcher::demux::Demultiplexer;
 use dispatcher::request::{RequestHandle, Request};
-use reactor::{Event, EventDemultiplexer, EventHandler, Reactor};
+use reactor::{Event, EventDemultiplexer, EventHandler, Reactor, SharedData};
 use dispatcher::response::ResponseSender;
 
 pub struct RequestReactor {
-    handlers: BTreeMap<RequestHandle, Box<EventHandler<Request, ContextMap>>>,
+    handlers: BTreeMap<RequestHandle, Box<for<'a> EventHandler<Request, SharedData<'a>>>>,
     demultiplexer: Demultiplexer<Request>,
     context_map: ContextMap,
     responder: Box<ResponseSender>
@@ -24,19 +24,20 @@ impl RequestReactor {
     }
 }
 
-impl Reactor<ContextMap> for RequestReactor {
+impl Reactor for RequestReactor {
     type Event = Request;
     fn handle_events(&mut self) {
+        let mut shared_data = SharedData::new(&mut self.context_map, &mut *self.responder);
         while let Some(request) = self.demultiplexer.select() {
             trace!("RequestReactor: got event");
             if let Some(handler) = self.handlers.get_mut(&request.handle()) {
-                handler.handle_event(request, &mut self.context_map);
+                handler.handle_event(request, &mut shared_data);
             } else {
                 trace!("RequestReactor: no handler found for event");
             }
         }
     }
-    fn register_handler(&mut self, handler: Box<EventHandler<Self::Event, ContextMap>>) {
+    fn register_handler(&mut self, handler: Box<for<'a> EventHandler<Self::Event, SharedData<'a>>>) {
         self.handlers.insert(handler.handle(), handler);
     }
     fn remove_handler_by_handle(&mut self, handler: &RequestHandle) {
