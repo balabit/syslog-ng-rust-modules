7b965095d4d8a8266ff38b4386a72e4f40981cc6
Tibor Benke <ihrwein@gmail.com>
2016-02-01 19:00:24 +0100

    MessageEventHandler: use higher-ranked lifetime
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/dispatcher/handlers/message.rs
+++ src/dispatcher/handlers/message.rs
@@ -1,7 +1,6 @@
 use dispatcher::request::{Request, RequestHandle};
-use context::ContextMap;
 use context::context_map::StreamingIterator;
-use reactor;
+use reactor::{EventHandler, SharedData};
 
 pub struct MessageEventHandler;
 
@@ -11,12 +10,12 @@ impl MessageEventHandler {
     }
 }
 
-impl reactor::EventHandler<Request, ContextMap> for MessageEventHandler {
-    fn handle_event(&mut self, event: Request, data: &mut ContextMap) {
+impl<'a> EventHandler<Request, SharedData<'a>> for MessageEventHandler {
+    fn handle_event(&mut self, event: Request, data: &mut SharedData) {
         trace!("MessageEventHandler: handle_event()");
         if let Request::Message(event) = event {
             for i in event.ids() {
-                let mut iter = data.contexts_iter_mut(i);
+                let mut iter = data.map.contexts_iter_mut(i);
                 while let Some(context) = iter.next() {
                     context.on_event(Request::Message(event.clone()));
                 }
