82ca085f2b4a82efbf2e1ff8cdf759116d842ced
Tibor Benke <ihrwein@gmail.com>
2016-02-03 19:08:32 +0100

    action: remove unused from_config() function
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/action.rs
+++ src/action.rs
@@ -1,4 +1,3 @@
-use config::action::ActionType;
 use state::State;
 use dispatcher::response::ResponseSender;
 use context::base::BaseContext;
@@ -8,10 +7,4 @@ pub use config::action::message::Alert;
 pub trait Action {
     fn on_opened(&self, state: &State, context: &BaseContext, &mut ResponseSender);
     fn on_closed(&self, state: &State, context: &BaseContext, &mut ResponseSender);
-}
-
-pub fn from_config(config: ActionType) -> Box<Action> {
-    match config {
-        ActionType::Message(action) => Box::new(action),
-    }
 }
