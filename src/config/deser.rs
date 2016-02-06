abd5c40d37b9307c8d4354ffef9ac376fd870d09
Tibor Benke <ihrwein@gmail.com>
2016-02-06 21:39:55 +0100

    ContextConfig: add new unit test for deserialization
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/config/deser.rs
+++ src/config/deser.rs
@@ -280,4 +280,19 @@ mod test {
         assert_eq!(&expected_context_id,
                    &context.context_id.as_ref().unwrap().to_string());
     }
+
+    #[test]
+    fn test_given_config_context_when_the_context_id_is_invalid_template_then_the_config_cannot_be_deserialized() {
+        let text = r#"
+        {
+            "uuid": "86ca9f93-84fb-4813-b037-6526f7a585a3",
+            "context_id": "{invalid}}",
+            "conditions": {
+                "timeout": 100
+            }
+        }"#;
+
+        let result = from_str::<ContextConfig>(text);
+        let _ = result.err().unwrap();
+    }
 }
