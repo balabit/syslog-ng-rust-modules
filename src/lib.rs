2e6fc2bf51e8f640ce18a1a50f5096845fae98b9
Tibor Benke <ihrwein@gmail.com>
2016-03-02 21:10:39 +0100

    tests: rename LOGGEN_EXPR to _LOGGEN_EXPR
    So it won't cause warnings.
    
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -8,22 +8,22 @@ use syslog_ng_common::{LogMessage, Parser, ParserBuilder, OptionError};
 use regex::Regex;
 
 // Example: "seq: 0000000000, thread: 0000, runid: 1456947132, stamp: 2016-03-02T20:32:12 PAD"
-const LOGGEN_EXPR: &'static str = r"seq: (?P<seq>\d+), thread: (?P<thread>\d+), runid: (?P<runid>\d+), stamp: (?P<stamp>[^ ]+) (?P<padding>.*$)";
+const _LOGGEN_EXPR: &'static str = r"seq: (?P<seq>\d+), thread: (?P<thread>\d+), runid: (?P<runid>\d+), stamp: (?P<stamp>[^ ]+) (?P<padding>.*$)";
 
 #[test]
 fn test_loggen_regex_can_be_compiled() {
-    let _ = Regex::new(LOGGEN_EXPR).unwrap();
+    let _ = Regex::new(_LOGGEN_EXPR).unwrap();
 }
 
 #[test]
 fn test_syslog_regex_accepts_valid_syslog_message() {
-    let re = Regex::new(LOGGEN_EXPR).unwrap();
+    let re = Regex::new(_LOGGEN_EXPR).unwrap();
     assert_eq!(true, re.is_match("seq: 0000000000, thread: 0000, runid: 1456947132, stamp: 2016-03-02T20:32:12 PAD"));
 }
 
 #[test]
 fn test_syslog_regex_parses_syslog_message() {
-    let re = Regex::new(LOGGEN_EXPR).unwrap();
+    let re = Regex::new(_LOGGEN_EXPR).unwrap();
     let caps = re.captures("seq: 0000000000, thread: 0000, runid: 1456947132, stamp: 2016-03-02T20:32:12 PAD").unwrap();
     assert_eq!("0000000000", caps.name("seq").unwrap());
     assert_eq!("0000", caps.name("thread").unwrap());
