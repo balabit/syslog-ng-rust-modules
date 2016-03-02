89a1322a18dc4d6e8fb4bff30ab35256cb9fd1b4
Tibor Benke <ihrwein@gmail.com>
2016-03-02 21:21:42 +0100

    tests: move tests into their own module
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -11,26 +11,32 @@ use regex::Regex;
 const _LOGGEN_EXPR: &'static str = r"seq: (?P<seq>\d+), thread: (?P<thread>\d+), runid: (?P<runid>\d+), stamp: (?P<stamp>[^ ]+) (?P<padding>.*$)";
 const REGEX_OPTION: &'static str = "regex";
 
-#[test]
-fn test_loggen_regex_can_be_compiled() {
-    let _ = Regex::new(_LOGGEN_EXPR).unwrap();
-}
+#[cfg(test)]
+mod tests {
+    use regex::Regex;
+    use super::_LOGGEN_EXPR;
 
-#[test]
-fn test_syslog_regex_accepts_valid_syslog_message() {
-    let re = Regex::new(_LOGGEN_EXPR).unwrap();
-    assert_eq!(true, re.is_match("seq: 0000000000, thread: 0000, runid: 1456947132, stamp: 2016-03-02T20:32:12 PAD"));
-}
+    #[test]
+    fn test_loggen_regex_can_be_compiled() {
+        let _ = Regex::new(_LOGGEN_EXPR).unwrap();
+    }
 
-#[test]
-fn test_syslog_regex_parses_syslog_message() {
-    let re = Regex::new(_LOGGEN_EXPR).unwrap();
-    let caps = re.captures("seq: 0000000000, thread: 0000, runid: 1456947132, stamp: 2016-03-02T20:32:12 PAD").unwrap();
-    assert_eq!("0000000000", caps.name("seq").unwrap());
-    assert_eq!("0000", caps.name("thread").unwrap());
-    assert_eq!("1456947132", caps.name("runid").unwrap());
-    assert_eq!("2016-03-02T20:32:12", caps.name("stamp").unwrap());
-    assert_eq!("PAD", caps.name("padding").unwrap());
+    #[test]
+    fn test_syslog_regex_accepts_valid_syslog_message() {
+        let re = Regex::new(_LOGGEN_EXPR).unwrap();
+        assert_eq!(true, re.is_match("seq: 0000000000, thread: 0000, runid: 1456947132, stamp: 2016-03-02T20:32:12 PAD"));
+    }
+
+    #[test]
+    fn test_syslog_regex_parses_syslog_message() {
+        let re = Regex::new(_LOGGEN_EXPR).unwrap();
+        let caps = re.captures("seq: 0000000000, thread: 0000, runid: 1456947132, stamp: 2016-03-02T20:32:12 PAD").unwrap();
+        assert_eq!("0000000000", caps.name("seq").unwrap());
+        assert_eq!("0000", caps.name("thread").unwrap());
+        assert_eq!("1456947132", caps.name("runid").unwrap());
+        assert_eq!("2016-03-02T20:32:12", caps.name("stamp").unwrap());
+        assert_eq!("PAD", caps.name("padding").unwrap());
+    }
 }
 
 #[derive(Clone)]
