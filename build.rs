9b7091f041331b7e9d09b0b44db8ba0c3f62d6b0
Tibor Benke <ihrwein@gmail.com>
2016-03-11 21:18:08 +0100

    Update build.rs

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ extern crate syslog_ng_build;
 
 fn main() {
     let canonical_name = "regex-parser";
-    let description = "This is syslog header parser written in Rust";
+    let description = "This is regex based log parser written in Rust";
     let parser_name = "regex-rs";
     syslog_ng_build::create_module(canonical_name, description, Some(parser_name));
 }
