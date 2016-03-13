4e2d4ee423d34240544e096ba00094dd16653c08
Tibor Benke <ihrwein@gmail.com>
2016-03-13 22:16:38 +0100

    tests: remove unneeded unit test
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- _test_module/__init__.py
+++ _test_module/__init__.py
@@ -1,16 +1,6 @@
-class ParserForImport:
-    def init(self, options):
-        pass
-    def parse(self, logmsg, message):
-        print("foo")
-        print("bar")
-        print(message)
-        logmsg["foo"] = "bar"
-        print(logmsg["foo"])
-        return True
-
 # Keep this class commented out
 # class NonExistingParser: pass
+
 class ExistingParser: pass
 
 class CallableClass: pass
--- tests/parser_builder.rs
+++ tests/parser_builder.rs
@@ -5,8 +5,7 @@ extern crate env_logger;
 
 use std::env;
 use python_parser::{PythonParserBuilder, options};
-use syslog_ng_common::{ParserBuilder, Parser, LogMessage};
-use syslog_ng_common::sys::logmsg::log_msg_registry_init;
+use syslog_ng_common::ParserBuilder;
 use cpython::{Python, PyDict};
 
 const TEST_MODULE_NAME: &'static str = "_test_module";
@@ -146,18 +145,4 @@ fn test_exception_is_raised_in_init_method() {
     builder.option(options::MODULE.to_owned(), "_test_module".to_owned());
     builder.option(options::CLASS.to_owned(), "ExceptionIsRaisedInInitMethod".to_owned());
     let _ = builder.build().err().unwrap();
-}
-
-#[test]
-fn test_parser_parses_the_message() {
-    unsafe {
-        log_msg_registry_init();
-    };
-    env::set_var("PYTHONPATH", env::current_dir().unwrap());
-    let mut builder = PythonParserBuilder::new();
-    builder.option(options::MODULE.to_owned(), "_test_module".to_owned());
-    builder.option(options::CLASS.to_owned(), "ParserForImport".to_owned());
-    let mut parser = builder.build().unwrap();
-    let mut logmsg = LogMessage::new();
-    let _ = parser.parse(&mut logmsg, "input message for parse method");
 }
