a957c462615e049e66a99edd7fada00ca4c898e1
Tibor Benke <ihrwein@gmail.com>
2016-03-23 21:17:05 +0100

    src: update syslog-ng-common to 0.7
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,8 @@ extern crate syslog_ng_common;
 extern crate log;
 extern crate regex;
 
-use syslog_ng_common::{LogMessage, Parser, ParserBuilder, OptionError};
+use std::marker::PhantomData;
+use syslog_ng_common::{LogMessage, Parser, ParserBuilder, OptionError, Pipe};
 use regex::Regex;
 
 // Example: "seq: 0000000000, thread: 0000, runid: 1456947132, stamp: 2016-03-02T20:32:12 PAD"
@@ -19,14 +20,15 @@ pub struct RegexParser {
     pub regex: Regex,
 }
 
-pub struct RegexParserBuilder {
+pub struct RegexParserBuilder<P: Pipe> {
     regex: Option<Regex>,
+    _marker: PhantomData<P>
 }
 
-impl ParserBuilder for RegexParserBuilder {
+impl<P: Pipe> ParserBuilder<P> for RegexParserBuilder<P> {
     type Parser = RegexParser;
     fn new() -> Self {
-        RegexParserBuilder { regex: None }
+        RegexParserBuilder { regex: None, _marker: PhantomData }
     }
     fn option(&mut self, name: String, value: String) {
         if name == REGEX_OPTION {
@@ -47,8 +49,8 @@ impl ParserBuilder for RegexParserBuilder {
     }
 }
 
-impl Parser for RegexParser {
-    fn parse(&mut self, logmsg: &mut LogMessage, input: &str) -> bool {
+impl<P: Pipe> Parser<P> for RegexParser {
+    fn parse(&mut self, _: &mut P, logmsg: &mut LogMessage, input: &str) -> bool {
         if let Some(captures) = self.regex.captures(input) {
             for (name, value) in captures.iter_named() {
                 if let Some(value) = value {
@@ -62,4 +64,4 @@ impl Parser for RegexParser {
     }
 }
 
-parser_plugin!(RegexParserBuilder);
+parser_plugin!(RegexParserBuilder<LogParser>);
--- src/tests.rs
+++ src/tests.rs
@@ -2,7 +2,7 @@ use regex::Regex;
 use super::*;
 
 use syslog_ng_common::sys::logmsg::log_msg_registry_init;
-use syslog_ng_common::{LogMessage, Parser, ParserBuilder, OptionError};
+use syslog_ng_common::{LogMessage, Parser, ParserBuilder, OptionError, mock};
 
 #[test]
 fn test_loggen_regex_can_be_compiled() {
@@ -39,9 +39,10 @@ fn test_parse_inserts_parsed_named_captures_into_the_logmsg() {
     let loggen_regex = Regex::new(LOGGEN_EXPR).unwrap();
     let mut parser = RegexParser { regex: loggen_regex };
     let mut logmsg = LogMessage::new();
+    let mut pipe = mock::MockPipe::new();
     let input = "seq: 0000000000, thread: 0000, runid: 1456947132, stamp: 2016-03-02T20:32:12 \
                  PAD";
-    parser.parse(&mut logmsg, input);
+    parser.parse(&mut pipe, &mut logmsg, input);
     assert_eq!("0000000000", logmsg.get("seq"));
     assert_eq!("0000", logmsg.get("thread"));
     assert_eq!("1456947132", logmsg.get("runid"));
@@ -51,14 +52,14 @@ fn test_parse_inserts_parsed_named_captures_into_the_logmsg() {
 
 #[test]
 fn test_parser_can_be_built_with_valid_regex() {
-    let mut builder = RegexParserBuilder::new();
+    let mut builder = RegexParserBuilder::<mock::MockPipe>::new();
     builder.option(REGEX_OPTION.to_string(), "[abc]d".to_string());
     let _ = builder.build().unwrap();
 }
 
 #[test]
 fn test_parser_cannot_be_built_with_invalid_regex() {
-    let mut builder = RegexParserBuilder::new();
+    let mut builder = RegexParserBuilder::<mock::MockPipe>::new();
     builder.option(REGEX_OPTION.to_string(), "[abcd".to_string());
     assert_eq!(Some(OptionError::missing_required_option(REGEX_OPTION)), builder.build().err());
 }
