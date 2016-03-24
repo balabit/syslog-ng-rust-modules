361cc4812753d1535e9d5422ca138ae2ed27fc54
Tibor Benke <tibor.benke@balabit.com>
2016-03-24 11:45:21 +0100

    modules: add regex-parser
    (same as a957c462615e049e66a99edd7fada00ca4c898e1 in https://github.com/ihrwein/regex-parser.git)
    
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

---
+++ regex-parser/src/lib.rs
@@ -0,0 +1,67 @@
+#[macro_use]
+extern crate syslog_ng_common;
+#[macro_use]
+extern crate log;
+extern crate regex;
+
+use std::marker::PhantomData;
+use syslog_ng_common::{LogMessage, Parser, ParserBuilder, OptionError, Pipe};
+use regex::Regex;
+
+// Example: "seq: 0000000000, thread: 0000, runid: 1456947132, stamp: 2016-03-02T20:32:12 PAD"
+pub const LOGGEN_EXPR: &'static str = r"seq: (?P<seq>\d+), thread: (?P<thread>\d+), runid: (?P<runid>\d+), stamp: (?P<stamp>[^ ]+) (?P<padding>.*$)";
+pub const REGEX_OPTION: &'static str = "regex";
+
+#[cfg(test)]
+mod tests;
+
+#[derive(Clone)]
+pub struct RegexParser {
+    pub regex: Regex,
+}
+
+pub struct RegexParserBuilder<P: Pipe> {
+    regex: Option<Regex>,
+    _marker: PhantomData<P>
+}
+
+impl<P: Pipe> ParserBuilder<P> for RegexParserBuilder<P> {
+    type Parser = RegexParser;
+    fn new() -> Self {
+        RegexParserBuilder { regex: None, _marker: PhantomData }
+    }
+    fn option(&mut self, name: String, value: String) {
+        if name == REGEX_OPTION {
+            debug!("Trying to compile regular expression: '{}'", &value);
+            match Regex::new(&value) {
+                Ok(regex) => self.regex = Some(regex),
+                Err(err) => error!("{}", err)
+            }
+        }
+    }
+    fn build(self) -> Result<Self::Parser, OptionError> {
+        debug!("Building Regex parser");
+        if let Some(regex) = self.regex {
+            Ok(RegexParser { regex: regex })
+        } else {
+            Err(OptionError::missing_required_option(REGEX_OPTION))
+        }
+    }
+}
+
+impl<P: Pipe> Parser<P> for RegexParser {
+    fn parse(&mut self, _: &mut P, logmsg: &mut LogMessage, input: &str) -> bool {
+        if let Some(captures) = self.regex.captures(input) {
+            for (name, value) in captures.iter_named() {
+                if let Some(value) = value {
+                    logmsg.insert(name, value);
+                }
+            }
+            true
+        } else {
+            false
+        }
+    }
+}
+
+parser_plugin!(RegexParserBuilder<LogParser>);
