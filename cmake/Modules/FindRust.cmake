b9f1e4da81cbf3feff809e756a8afcf576aa581f
Tibor Benke <tibor.benke@balabit.com>
2016-03-25 15:12:02 +0100

    cmake: update cmake to work withput multirust

--- cmake/Modules/FindRust.cmake
+++ cmake/Modules/FindRust.cmake
@@ -12,7 +12,11 @@
 
 include(CheckMultirust)
 
-find_program(RUST_EXECUTABLE rustc HINTS ${MULTIRUST_TOOLCHAIN_BIN_DIR} PATHS PATH_SUFFIXES bin)
+if (MULTIRUST_FOUND)
+  find_program(RUST_EXECUTABLE rustc HINTS ${MULTIRUST_TOOLCHAIN_BIN_DIR} PATHS PATH_SUFFIXES bin)
+else()
+  find_program(RUST_EXECUTABLE rustc PATHS PATH_SUFFIXES bin)
+endif()
 
 if (RUST_EXECUTABLE)
     set(COMMAND ${RUST_EXECUTABLE} --version)
