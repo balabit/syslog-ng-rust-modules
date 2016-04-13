9a9e2b531ff1275de83613b8edd66d334e73a58f
Tibor Benke <ihrwein@gmail.com>
2016-04-13 11:02:13 +0200

    correlation: add template test
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- correlation/src/config/action/message/test.rs
+++ correlation/src/config/action/message/test.rs
@@ -22,7 +22,7 @@ use std::sync::Arc;
 use uuid::Uuid;
 use Event;
 use Message;
-use test_utils::{MockTemplate, MockTemplateFactory};
+use test_utils::{MockTemplate};
 
 #[test]
 fn test_given_message_action_when_it_is_executed_then_the_additional_values_are_inserted_into_the_generated_message
@@ -49,6 +49,40 @@ fn test_given_message_action_when_it_is_executed_then_the_additional_values_are_
     let responses = responder.0;
     if let Response::Alert(ref response) = *responses.get(0).unwrap() {
         let message = &response.message;
+        assert_eq!("value1",
+                   message.get("key1").expect("Failed to get an additional key-value pair from a generated message"));
+        assert_eq!("value2",
+                   message.get("key2").expect("Failed to get an additional key-value pair from a generated message"));
+    } else {
+        unreachable!();
+    }
+}
+
+#[test]
+fn test_executed_message_action_uses_the_templates() {
+    let mut responder = MockResponseSender::default();
+    let uuid_as_str = "2f34112c-6fc8-406b-a6f0-78158ca724b6";
+    let uuid = Uuid::parse_str(uuid_as_str).unwrap();
+    let base_context = {
+        let conditions = ConditionsBuilder::new(Duration::from_millis(100)).build();
+        BaseContextBuilder::<Message, MockTemplate>::new(uuid.clone(), conditions).name(Some("name".to_owned())).build()
+    };
+    let state = {
+        let messages = vec![Arc::new(MessageBuilder::new("uuid1", "message1").build()),
+                            Arc::new(MessageBuilder::new("uuid2", "message2").build())];
+        State::with_messages(messages)
+    };
+    let message_action = MessageActionBuilder::<MockTemplate>::new("uuid", MockTemplate::literal(uuid_as_str))
+                                              .pair("key1", MockTemplate::literal("value1"))
+                                              .pair("key2", MockTemplate::literal("value2"))
+                                              .build();
+
+    message_action.on_closed(&state, &base_context, &mut responder);
+    assert_eq!(1, responder.0.len());
+    let responses = responder.0;
+    if let Response::Alert(ref response) = *responses.get(0).unwrap() {
+        let message = &response.message;
+        assert_eq!(uuid_as_str, message.message());
         assert_eq!("value1",
                    message.get("key1").expect("Failed to get an additional key-value pair from a generated message"));
         assert_eq!("value2",
