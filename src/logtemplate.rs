23912edc50d0f9844b98caab9675efcdb4716d1f
Tibor Benke <ihrwein@gmail.com>
2016-04-15 14:22:45 +0200

    LogTemplate: implement format_with_context()
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/logtemplate.rs
+++ src/logtemplate.rs
@@ -1,18 +1,23 @@
 use std::sync::Arc;
+use std::fmt::Write;
+use std::cell::RefCell;
 
-use syslog_ng_common::{self, GlobalConfig};
+use syslog_ng_common::{self, GlobalConfig, LogTimeZone};
 
 use logevent::LogEvent;
 use correlation::{Template, TemplateFactory, CompileError};
 
 unsafe impl Send for LogTemplate {}
 
-pub struct LogTemplate(syslog_ng_common::LogTemplate);
+pub struct LogTemplate(RefCell<syslog_ng_common::LogTemplate>);
 
 impl Template for LogTemplate {
     type Event = LogEvent;
-    fn format_with_context(&self, _: &[Arc<Self::Event>], _: &str, _: &mut String) {
-
+    fn format_with_context(&self, messages: &[Arc<Self::Event>], context_id: &str, buffer: &mut String) {
+        let messages: Vec<syslog_ng_common::LogMessage> = messages.iter().map(|event| event.0.clone()).collect();
+        let mut template = self.0.borrow_mut();
+        let formatted_str = template.format_with_context(&messages, None, LogTimeZone::Send, 0, Some(context_id));
+        let _ = buffer.write_str(formatted_str);
     }
 }
 
@@ -20,8 +25,10 @@ pub struct LogTemplateFactory(GlobalConfig);
 
 impl TemplateFactory<LogEvent> for LogTemplateFactory {
     type Template = LogTemplate;
-    fn compile(&self, _: &str) -> Result<Self::Template, CompileError> {
-        Err(CompileError("wtf".to_owned()))
+    fn compile(&self, value: &str) -> Result<Self::Template, CompileError> {
+        syslog_ng_common::LogTemplate::compile(&self.0, value)
+                                      .map(|template| LogTemplate(RefCell::new(template)))
+                                      .map_err(|err| CompileError(err.to_string()))
     }
 }
 
