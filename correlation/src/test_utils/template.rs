ihrwein/correlation-parser

add context_id and context_len mocking

correlation/src/test_utils/template.rs
@@ -30,11 +30,29 @@ impl Mock for LiteralMockTemplate {
     }
 }
 
+fn context_id(_: &[Arc<Message>], context_id: &str, buffer: &mut String) {
+    let _ = buffer.write_str(context_id);
+}
+
+fn context_len(messages: &[Arc<Message>], _: &str, buffer: &mut String) {
+    let _ = buffer.write_fmt(format_args!("{}", messages.len()));
+}
+
 impl MockTemplate {
     // return a literal from format()
     pub fn literal(value: &str) -> MockTemplate {
         MockTemplate {
             with_context: Box::new(LiteralMockTemplate(value.to_owned())),
+        }
+    }
+    pub fn contetxt_id() -> MockTemplate {
+        MockTemplate {
+            with_context: Box::new(context_id),
+        }
+    }
+    pub fn contetxt_len() -> MockTemplate {
+        MockTemplate {
+            with_context: Box::new(context_len),
         }
     }
 }
@@ -92,5 +110,23 @@ fn test_mock_template_returns_the_expected_literal() {
     let dummy_context_id = "doesn't matter";
     let mut actual = String::new();
     template.format_with_context(&[], dummy_context_id, &mut actual);
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn test_mock_template_can_return_context_id() {
+    let context_id = "79ace9c4-0693-4d5b-97d8-de39322bc64d";
+    let template = MockTemplate::contetxt_id();
+    let mut actual = String::new();
+    template.format_with_context(&[], context_id, &mut actual);
+    assert_eq!(context_id, actual);
+}
+
+#[test]
+fn test_mock_template_can_return_context_length() {
+    let expected = "0";
+    let template = MockTemplate::contetxt_len();
+    let mut actual = String::new();
+    template.format_with_context(&[], "doesn't matter", &mut actual);
     assert_eq!(expected, actual);
 }

balabit/syslog-ng-rust-modules

correlation/src/test_utils/template.rs
@@ -30,11 +30,29 @@ impl Mock for LiteralMockTemplate {
     }
 }
 
+fn context_id(_: &[Arc<Message>], context_id: &str, buffer: &mut String) {
+    let _ = buffer.write_str(context_id);
+}
+
+fn context_len(messages: &[Arc<Message>], _: &str, buffer: &mut String) {
+    let _ = buffer.write_fmt(format_args!("{}", messages.len()));
+}
+
 impl MockTemplate {
     // return a literal from format()
     pub fn literal(value: &str) -> MockTemplate {
         MockTemplate {
             with_context: Box::new(LiteralMockTemplate(value.to_owned())),
+        }
+    }
+    pub fn context_id() -> MockTemplate {
+        MockTemplate {
+            with_context: Box::new(context_id),
+        }
+    }
+    pub fn context_len() -> MockTemplate {
+        MockTemplate {
+            with_context: Box::new(context_len),
         }
     }
 }
@@ -92,5 +110,23 @@ fn test_mock_template_returns_the_expected_literal() {
     let dummy_context_id = "doesn't matter";
     let mut actual = String::new();
     template.format_with_context(&[], dummy_context_id, &mut actual);
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn test_mock_template_can_return_context_id() {
+    let context_id = "79ace9c4-0693-4d5b-97d8-de39322bc64d";
+    let template = MockTemplate::context_id();
+    let mut actual = String::new();
+    template.format_with_context(&[], context_id, &mut actual);
+    assert_eq!(context_id, actual);
+}
+
+#[test]
+fn test_mock_template_can_return_context_length() {
+    let expected = "0";
+    let template = MockTemplate::context_len();
+    let mut actual = String::new();
+    template.format_with_context(&[], "doesn't matter", &mut actual);
     assert_eq!(expected, actual);
 }