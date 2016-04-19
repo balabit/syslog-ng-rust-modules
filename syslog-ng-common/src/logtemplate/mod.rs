904acced1ab31df19766bca5c1f0a218b6df4d3a
Tibor Benke <tibor.benke@balabit.com>
2016-04-19 09:56:53 +0200

    syslog-ng-common: move tests out from logtemplate module
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- syslog-ng-common/src/logtemplate/mod.rs
+++ syslog-ng-common/src/logtemplate/mod.rs
@@ -8,6 +8,9 @@ use GlobalConfig;
 
 use std::ffi::{CString, CStr};
 
+#[cfg(test)]
+mod tests;
+
 pub struct LogTemplate {
     pub wrapped: *mut sys::LogTemplate,
     buffer: *mut glib_sys::GString,
@@ -87,67 +90,5 @@ impl Drop for LogTemplate {
             sys::log_template_unref(self.wrapped);
             glib_sys::g_string_free(self.buffer, 1 as glib_sys::gboolean);
         };
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use GlobalConfig;
-    use LogMessage;
-
-    use SYSLOG_NG_INITIALIZED;
-    use syslog_ng_global_init;
-
-    #[test]
-    fn test_template_can_be_created() {
-        SYSLOG_NG_INITIALIZED.call_once(|| {
-            unsafe { syslog_ng_global_init(); }
-        });
-        let cfg = GlobalConfig::new(0x0308);
-        let _ = LogTemplate::new(&cfg);
-    }
-
-    #[test]
-    fn test_template_can_be_compiled() {
-        SYSLOG_NG_INITIALIZED.call_once(|| {
-            unsafe { syslog_ng_global_init(); }
-        });
-        let cfg = GlobalConfig::new(0x0308);
-        let _ = LogTemplate::compile(&cfg, "literal").ok().unwrap();
-    }
-
-    #[test]
-    fn test_invalid_template_cannot_be_compiled() {
-        SYSLOG_NG_INITIALIZED.call_once(|| {
-            unsafe { syslog_ng_global_init(); }
-        });
-        let cfg = GlobalConfig::new(0x0308);
-        let _ = LogTemplate::compile(&cfg, "${unbalanced").err().unwrap();
-    }
-
-    #[test]
-    fn test_log_message_can_be_formatted() {
-        SYSLOG_NG_INITIALIZED.call_once(|| {
-            unsafe { syslog_ng_global_init(); }
-        });
-        let cfg = GlobalConfig::new(0x0308);
-        let mut template = LogTemplate::compile(&cfg, "${kittens}").ok().unwrap();
-        let mut msg = LogMessage::new();
-        msg.insert("kittens", "2");
-        let formatted_msg = template.format(&msg, None, LogTimeZone::Local, 0, None);
-        assert_eq!("2", formatted_msg);
-    }
-
-    #[test]
-    fn test_context_id_can_be_used() {
-        SYSLOG_NG_INITIALIZED.call_once(|| {
-            unsafe { syslog_ng_global_init(); }
-        });
-        let cfg = GlobalConfig::new(0x0308);
-        let mut template = LogTemplate::compile(&cfg, "${CONTEXT_ID}").ok().unwrap();
-        let msg = LogMessage::new();
-        let actual = template.format(&msg, None, LogTimeZone::Local, 0, Some("context-id"));
-        assert_eq!("context-id", actual);
     }
 }
