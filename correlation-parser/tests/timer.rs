97fb40de49b374b65e3a64a7b145b7bce935e9e5
Tibor Benke <tibor.benke@balabit.com>
2016-06-24 13:21:33 +0200

    correlation-parser: add tests for Watchdog
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

---
+++ correlation-parser/tests/timer.rs
@@ -0,0 +1,37 @@
+extern crate correlation_parser;
+
+use correlation_parser::{Timer, Watchdog};
+use correlation_parser::mock::{MockEvent, MockLogTemplate};
+
+use std::time::Duration;
+use std::sync::{Arc, Mutex};
+
+fn assert_callback_called(cb_interval: Duration, iter_count: u32, acceptable_bias: u32) {
+    let counter = Arc::new(Mutex::new(0));
+
+    let cloned_counter = counter.clone();
+    let timer = Watchdog::schedule(cb_interval, move || {
+        if let Ok(mut guard) = cloned_counter.lock() {
+            *guard += 1;
+        }
+    });
+
+    Timer::<MockEvent, MockLogTemplate>::start(&timer);
+    ::std::thread::sleep(cb_interval * iter_count);
+    Timer::<MockEvent, MockLogTemplate>::stop(&timer);
+
+    if let Ok(guard) = counter.lock() {
+        let less = *guard < (iter_count - acceptable_bias);
+        let greater = *guard > (iter_count + acceptable_bias);
+        let in_middle = !less && !greater;
+        assert!(in_middle);
+    } else {
+        unreachable!();
+    };
+}
+
+#[test]
+fn test_watchdog_calls_the_provided_callback_after_it_is_started() {
+    assert_callback_called(Duration::from_millis(50), 6, 1);
+    assert_callback_called(Duration::from_millis(50), 60, 5);
+}
