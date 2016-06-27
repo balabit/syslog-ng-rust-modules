70e765822e4b7e5242fc797f83674590d50512fa
Tibor Benke <tibor.benke@balabit.com>
2016-06-27 11:16:35 +0200

    correlation-parser: add some more tests
    * stop() works
    * start() works
    * timer can be stopped if
      * it's created
      * it's started but not stopped
      * it's stopped
    
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- correlation-parser/tests/timer.rs
+++ correlation-parser/tests/timer.rs
@@ -41,3 +41,59 @@ fn test_watchdog_calls_the_provided_callback_after_it_is_started() {
     assert_callback_called(Duration::from_millis(50), 6, 1);
     assert_callback_called(Duration::from_millis(50), 60, 5);
 }
+
+#[test]
+fn test_timer_is_started_after_start_call() {
+    let cb_interval = Duration::from_millis(50);
+    let (counter, timer) = create_counter_and_timer(cb_interval);
+
+    ::std::thread::sleep(cb_interval * 2);
+    assert_eq!(*counter.lock().unwrap(), 0);
+
+    Timer::<MockEvent, MockLogTemplate>::start(&timer);
+    // wait some deltas to let the timer thread start and increment the counter
+    ::std::thread::sleep(cb_interval * 2);
+    assert!(*counter.lock().unwrap() > 0);
+}
+
+#[test]
+fn test_timer_is_stopped_after_stop_call() {
+    let cb_interval = Duration::from_millis(50);
+    let (counter, timer) = create_counter_and_timer(cb_interval);
+
+    Timer::<MockEvent, MockLogTemplate>::start(&timer);
+    // wait some deltas to let the timer thread start and increment the counter
+    ::std::thread::sleep(cb_interval * 2);
+
+    // wait some deltas to make sure timer thread stopped
+    Timer::<MockEvent, MockLogTemplate>::stop(&timer);
+
+    let counter_value_after_stop = *counter.lock().unwrap();
+    // wait some deltas to make sure timer thread stopped
+    ::std::thread::sleep(cb_interval * 2);
+
+    assert_eq!(counter_value_after_stop, *counter.lock().unwrap());
+}
+
+#[test]
+fn test_freshly_created_timer_is_stopped_on_drop() {
+    let cb_interval = Duration::from_millis(50);
+    let _ = create_counter_and_timer(cb_interval);
+}
+
+#[test]
+fn test_started_timer_is_stopped_on_drop() {
+    let cb_interval = Duration::from_millis(50);
+    let (_, timer) = create_counter_and_timer(cb_interval);
+
+    Timer::<MockEvent, MockLogTemplate>::start(&timer);
+}
+
+#[test]
+fn test_stopped_timer_is_stopped_on_drop() {
+    let cb_interval = Duration::from_millis(50);
+    let (_, timer) = create_counter_and_timer(cb_interval);
+
+    Timer::<MockEvent, MockLogTemplate>::start(&timer);
+    Timer::<MockEvent, MockLogTemplate>::stop(&timer);
+}
