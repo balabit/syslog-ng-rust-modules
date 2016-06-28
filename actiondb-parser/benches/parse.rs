6162b4a64c84361fe6f020c0bcce838cddd9e144
Tibor Benke <tibor.benke@balabit.com>
2016-06-28 09:57:39 +0200

    actiondb-parser: fix broken benchmark build
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- actiondb-parser/benches/parse.rs
+++ actiondb-parser/benches/parse.rs
@@ -7,22 +7,24 @@ extern crate syslog_ng_common;
 use test::Bencher;
 use syslog_ng_common::{LogMessage, Parser, ParserBuilder};
 
-use syslog_ng_common::sys::logmsg::log_msg_registry_init;
+use syslog_ng_common::{syslog_ng_global_init, SYSLOG_NG_INITIALIZED, GlobalConfig};
+use syslog_ng_common::mock::MockPipe;
 use actiondb_parser::ActiondbParserBuilder;
-use actiondb::matcher::suffix_array::SuffixArrayMatcherSuite;
+use actiondb::matcher::suffix_array::{SuffixArrayMatcherSuite};
 
 #[bench]
 fn bench_parse(b: &mut Bencher) {
-    unsafe {
-        // We have to initialize some global variables in syslog-ng
-        log_msg_registry_init();
-    };
+    SYSLOG_NG_INITIALIZED.call_once(|| {
+        unsafe { syslog_ng_global_init(); }
+    });
 
+    let cfg = GlobalConfig::new(0x0308);
     let pattern_file_path = "benches/loggen.json";
-    let mut builder: ActiondbParserBuilder<SuffixArrayMatcherSuite> = ActiondbParserBuilder::new();
+    let mut pipe = MockPipe::new();
+    let mut builder: ActiondbParserBuilder<SuffixArrayMatcherSuite> = ParserBuilder::<MockPipe>::new(cfg);
     builder.set_pattern_file(pattern_file_path);
-    let mut parser = builder.build().unwrap();
+    let mut parser = ParserBuilder::<MockPipe>::build(builder).unwrap();
     let mut logmsg = LogMessage::new();
     let input = "seq: 0000000000, thread: 0000, runid: 1456947132, stamp: 2016-03-02T20:32:12 PAD";
-    b.iter(|| parser.parse(&mut logmsg, input));
+    b.iter(|| parser.parse(&mut pipe, &mut logmsg, input));
 }
