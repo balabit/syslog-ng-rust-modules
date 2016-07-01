65067d42ccad5e34a98a9e6e0437221de17fc515
Tibor Benke <tibor.benke@balabit.com>
2016-07-01 12:49:33 +0200

    syslog-ng-common: make in native_parser_proxy_init() unwind safe
    * catch panic!
    * kill syslog-ng with abort()
    
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- syslog-ng-rs/syslog-ng-common/src/proxies/parser/mod.rs
+++ syslog-ng-rs/syslog-ng-common/src/proxies/parser/mod.rs
@@ -56,8 +56,19 @@ pub mod _parser_plugin {
 
     #[no_mangle]
     pub extern fn native_parser_proxy_init(this: &mut ParserProxy<$name>) -> c_int {
-        let result = this.init();
-        bool_to_int(result)
+        let mut wrapper = AssertUnwindSafe(this);
+
+        let unwind_safe_call = move || {
+            bool_to_int(wrapper.init())
+        };
+
+        match catch_unwind(unwind_safe_call) {
+            Ok(init_result) => init_result,
+            Err(error) => {
+                error!("native_parser_proxy_init() panicked, but the panic was cought: {:?}", error);
+                commit_suicide();
+            }
+        }
     }
 
     #[no_mangle]
