d1b6fc14f907b75460e05cb885936d6c9d44dc66
Tibor Benke <tibor.benke@balabit.com>
2016-07-01 12:40:55 +0200

    syslog-ng-common: test native_parser_proxy_free()
    * panic! should be cought
    * child process is killed with abort()
    
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- syslog-ng-rs/syslog-ng-common/tests/panicking-parser.rs
+++ syslog-ng-rs/syslog-ng-common/tests/panicking-parser.rs
@@ -63,7 +63,7 @@ impl<P: Pipe> Clone for PanickingParserBuilder<P> {
 // this verifies that the macro can be expanded
 parser_plugin!(PanickingParserBuilder<LogParser>);
 
-use _parser_plugin::native_parser_proxy_new;
+use _parser_plugin::{native_parser_proxy_new, native_parser_proxy_free};
 
 
 fn set_up_test() {
@@ -115,5 +115,15 @@ fn test_native_parser_proxy_new_wont_panic_even_if_the_proxy_panics() {
 
         let cfg = GlobalConfig::new(0x0308);
         let _ = native_parser_proxy_new(cfg.raw_ptr());
+    });
+}
+
+#[test]
+fn test_native_parser_proxy_free_wont_panic_even_if_the_proxy_panics() {
+    assert_child_commits_suicide(|| {
+        set_up_test();
+
+        let proxy = ParserProxy::with_parser_and_builder(None, Some(PanickingParser(PhantomData)));
+        let _ = native_parser_proxy_free(Box::new(proxy));
     });
 }
