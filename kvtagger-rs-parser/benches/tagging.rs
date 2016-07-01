c9c2eb4bbd5be5423b14cfa67ad62fbc467c43d3
Tibor Benke <tibor.benke@balabit.com>
2016-07-01 17:35:06 +0200

    kvtagger: add benchmark
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- kvtagger-rs-parser/benches/tagging.rs
+++ kvtagger-rs-parser/benches/tagging.rs
@@ -48,3 +48,23 @@ fn bench_parse_with_prefix(b: &mut Bencher) {
     let input = "<34>Oct 11 22:14:15 100.100.131.85 su: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
     b.iter(|| parser.parse(&mut pipe, &mut logmsg, input));
 }
+
+#[bench]
+fn bench_parse_with_fallback_to_default_selector(b: &mut Bencher) {
+    SYSLOG_NG_INITIALIZED.call_once(|| {
+        unsafe { syslog_ng_global_init(); }
+    });
+
+    let cfg = GlobalConfig::new(0x0308);
+    let csv_file_path = "benches/bigcsv.csv";
+    let mut pipe = MockPipe::new();
+    let mut builder: KVTaggerBuilder<MockPipe> = ParserBuilder::<MockPipe>::new(cfg.clone());
+    let selector = LogTemplate::compile(&cfg, "XXXXXXXXXX".as_bytes()).unwrap();
+    builder.set_csv_file(csv_file_path);
+    builder.set_lookup_key(selector);
+    builder.set_default_selector("7.209.203.75".to_string());
+    let mut parser = ParserBuilder::<MockPipe>::build(builder).unwrap();
+    let mut logmsg = LogMessage::new();
+    let input = "<34>Oct 11 22:14:15 100.100.131.85 su: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+    b.iter(|| parser.parse(&mut pipe, &mut logmsg, input));
+}
