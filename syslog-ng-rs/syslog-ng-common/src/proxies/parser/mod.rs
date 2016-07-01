c73160168d4fd5393b5b2b0bf42d1683ad39dc70
Tibor Benke <tibor.benke@balabit.com>
2016-07-01 12:52:05 +0200

    syslog-ng-common: make in native_parser_proxy_process() unwind safe
    * catch panic!
    * kill syslog-ng with abort()
    
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- syslog-ng-rs/syslog-ng-common/src/proxies/parser/mod.rs
+++ syslog-ng-rs/syslog-ng-common/src/proxies/parser/mod.rs
@@ -129,20 +129,36 @@ pub mod _parser_plugin {
 
     #[no_mangle]
     pub extern fn native_parser_proxy_process(this: &mut ParserProxy<$name>, parent: *mut $crate::sys::LogParser, msg: *mut $crate::sys::LogMessage, input: *const c_char) -> c_int {
-        let input = unsafe { CStr::from_ptr(input).to_str() };
-        let mut parent = LogParser::wrap_raw(parent);
-        let mut msg = LogMessage::wrap_raw(msg);
-        let result = match input {
-            Ok(input) => this.process(&mut parent, &mut msg, input),
-            Err(err) => {
-                error!("{}", err);
-                false
-            }
-        };
+        let mut wrapper_this = AssertUnwindSafe(this);
+        let wrapper_parent = AssertUnwindSafe(parent);
+        let wrapper_msg = AssertUnwindSafe(msg);
+        let wrapper_input = AssertUnwindSafe(input);
+
+        let result = catch_unwind(move || {
+            let input = unsafe { CStr::from_ptr(*wrapper_input).to_str() };
+
+            let result: bool = match input {
+                Ok(input) => {
+                    let mut parent = LogParser::wrap_raw(*wrapper_parent);
+                    let mut msg = LogMessage::wrap_raw(*wrapper_msg);
+
+                    wrapper_this.process(&mut parent, &mut msg, input)
+                },
+                Err(err) => {
+                    error!("{}", err);
+                    false
+                }
+            };
+
+            bool_to_int(result)
+        });
 
         match result {
-            true => 1,
-            false => 0
+            Ok(process_result) => process_result,
+            Err(error) => {
+                error!("native_parser_proxy_process() panicked, but the panic was cought: {:?}", error);
+                commit_suicide();
+            }
         }
     }
 
@@ -165,9 +181,16 @@ pub mod _parser_plugin {
     }
 
     #[no_mangle]
-    pub extern fn native_parser_proxy_clone(slf: &ParserProxy<$name>) -> Box<ParserProxy<$name>> {
-        let cloned = (*slf).clone();
-        Box::new(cloned)
+    pub extern fn native_parser_proxy_clone(this: &ParserProxy<$name>) -> *mut ParserProxy<$name> {
+        let wrapper_this = AssertUnwindSafe(this);
+
+        match catch_unwind(move || (*wrapper_this).clone()) {
+            Ok(cloned) => Box::into_raw(Box::new(cloned)),
+            Err(error) => {
+                error!("native_parser_proxy_clone() panicked, but the panic was caught: {:?}", error);
+                commit_suicide();
+            }
+        }
     }
 }
     }
