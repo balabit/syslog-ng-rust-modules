4ed52b0e3e367d9505e350fd96e11f5f43ec1158
Tibor Benke <ihrwein@gmail.com>
2016-07-07 08:03:39 +0200

    kvtagger: refactor benchmarks

--- kvtagger-rs-parser/benches/tagging.rs
+++ kvtagger-rs-parser/benches/tagging.rs
@@ -6,6 +6,7 @@ extern crate syslog_ng_common;
 use test::Bencher;
 use syslog_ng_common::{LogMessage, Parser, ParserBuilder};
 use kvtagger_rs_parser::{options, KVTaggerBuilder};
+use kvtagger_rs_parser::utils::build_parser;
 
 use syslog_ng_common::{syslog_ng_global_init, SYSLOG_NG_INITIALIZED, GlobalConfig, LogTemplate};
 use syslog_ng_common::mock::MockPipe;
@@ -17,13 +18,13 @@ fn bench_parse(b: &mut Bencher) {
     });
 
     let cfg = GlobalConfig::new(0x0308);
-    let csv_file_path = "benches/bigcsv.csv";
     let mut pipe = MockPipe::new();
-    let mut builder: KVTaggerBuilder<MockPipe> = ParserBuilder::<MockPipe>::new(cfg.clone());
-    let selector = LogTemplate::compile(&cfg, "7.209.203.75".as_bytes()).unwrap();
-    builder.set_database(csv_file_path);
-    builder.set_selector(selector);
-    let mut parser = ParserBuilder::<MockPipe>::build(builder).unwrap();
+    let options = [
+        (options::DATABASE, "benches/bigcsv.csv"),
+        (options::SELECTOR, "7.209.203.75")
+    ];
+
+    let mut parser = build_parser::<MockPipe, KVTaggerBuilder<_>>(cfg, &options).unwrap();
     let mut logmsg = LogMessage::new();
     let input = "<34>Oct 11 22:14:15 100.100.131.85 su: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
     b.iter(|| parser.parse(&mut pipe, &mut logmsg, input));
@@ -36,14 +37,15 @@ fn bench_parse_with_prefix(b: &mut Bencher) {
     });
 
     let cfg = GlobalConfig::new(0x0308);
-    let csv_file_path = "benches/bigcsv.csv";
     let mut pipe = MockPipe::new();
-    let mut builder: KVTaggerBuilder<MockPipe> = ParserBuilder::<MockPipe>::new(cfg.clone());
-    let selector = LogTemplate::compile(&cfg, "7.209.203.75".as_bytes()).unwrap();
-    builder.set_database(csv_file_path);
-    builder.set_selector(selector);
-    builder.set_prefix("prefix.".to_string());
-    let mut parser = ParserBuilder::<MockPipe>::build(builder).unwrap();
+
+    let options = [
+        (options::DATABASE, "benches/bigcsv.csv"),
+        (options::SELECTOR, "7.209.203.75"),
+        (options::PREFIX, "prefix."),
+    ];
+
+    let mut parser = build_parser::<MockPipe, KVTaggerBuilder<_>>(cfg, &options).unwrap();
     let mut logmsg = LogMessage::new();
     let input = "<34>Oct 11 22:14:15 100.100.131.85 su: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
     b.iter(|| parser.parse(&mut pipe, &mut logmsg, input));
@@ -56,14 +58,16 @@ fn bench_parse_with_fallback_to_default_selector(b: &mut Bencher) {
     });
 
     let cfg = GlobalConfig::new(0x0308);
-    let csv_file_path = "benches/bigcsv.csv";
     let mut pipe = MockPipe::new();
-    let mut builder: KVTaggerBuilder<MockPipe> = ParserBuilder::<MockPipe>::new(cfg.clone());
-    let selector = LogTemplate::compile(&cfg, "XXXXXXXXXX".as_bytes()).unwrap();
-    builder.set_database(csv_file_path);
-    builder.set_selector(selector);
-    builder.set_default_selector("7.209.203.75".to_string());
-    let mut parser = ParserBuilder::<MockPipe>::build(builder).unwrap();
+
+    let options = [
+        (options::DATABASE, "benches/bigcsv.csv"),
+        (options::SELECTOR, "XXXXXX"),
+        (options::DEFAULT_SELECTOR, "7.209.203.75"),
+    ];
+
+    let mut parser = build_parser::<MockPipe, KVTaggerBuilder<_>>(cfg, &options).unwrap();
+
     let mut logmsg = LogMessage::new();
     let input = "<34>Oct 11 22:14:15 100.100.131.85 su: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
     b.iter(|| parser.parse(&mut pipe, &mut logmsg, input));
