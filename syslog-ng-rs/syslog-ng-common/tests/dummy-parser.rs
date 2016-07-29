21d29ee0ea55b512c5773ca1dd687e68f8183058
Tibor Benke <tibor.benke@balabit.com>
2016-07-29 14:11:46 +0200

    syslog-ng-common: remove P: Pipe bounds from dummy-parser
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- syslog-ng-rs/syslog-ng-common/tests/dummy-parser.rs
+++ syslog-ng-rs/syslog-ng-common/tests/dummy-parser.rs
@@ -11,20 +11,19 @@ extern crate syslog_ng_common;
 #[macro_use]
 extern crate log;
 
-use std::marker::PhantomData;
 use std::ffi::CString;
 
-pub struct DummyParser;
+pub struct DummyParser {}
 
-pub struct DummyParserBuilder<P: Pipe>(PhantomData<P>);
+pub struct DummyParserBuilder {}
 
 use syslog_ng_common::LogMessage;
 use syslog_ng_common::{Parser, ParserBuilder, Error, Pipe, GlobalConfig};
 
-impl<P: Pipe> ParserBuilder<P> for DummyParserBuilder<P> {
+impl ParserBuilder for DummyParserBuilder {
     type Parser = DummyParser;
     fn new(_: GlobalConfig) -> Self {
-        DummyParserBuilder(PhantomData)
+        DummyParserBuilder{}
     }
     fn option(&mut self, name: String, value: String) -> Result<(), Error> {
         debug!("Setting option: {}={}", name, value);
@@ -35,28 +34,28 @@ impl<P: Pipe> ParserBuilder<P> for DummyParserBuilder<P> {
     }
     fn build(self) -> Result<Self::Parser, Error> {
         debug!("Building Rust parser");
-        Ok(DummyParser)
+        Ok(DummyParser{})
     }
 }
 
-impl<P: Pipe> Parser<P> for DummyParser {
-    fn parse(&mut self, _: &mut P, message: &mut LogMessage, input: &str) -> bool {
+impl Parser for DummyParser {
+    fn parse(&mut self, _: &mut Pipe, message: &mut LogMessage, input: &str) -> bool {
         debug!("Processing input in Rust Parser: {}", input);
         message.insert(&b"input"[..], input.as_bytes());
         true
     }
 }
 
-impl<P: Pipe> Clone for DummyParserBuilder<P> {
+impl Clone for DummyParserBuilder {
     fn clone(&self) -> Self {
-        DummyParserBuilder(PhantomData)
+        DummyParserBuilder{}
     }
 }
 
 // this verifies that the macro can be expanded
-parser_plugin!(DummyParserBuilder<LogParser>);
+parser_plugin!(DummyParserBuilder);
 
-use syslog_ng_common::{SYSLOG_NG_INITIALIZED, syslog_ng_global_init, ParserProxy, LogParser};
+use syslog_ng_common::{SYSLOG_NG_INITIALIZED, syslog_ng_global_init, ParserProxy};
 use syslog_ng_common::mock::MockPipe;
 
 #[test]
@@ -66,7 +65,7 @@ fn test_given_parser_implementation_when_it_receives_a_message_then_it_adds_a_sp
         unsafe { syslog_ng_global_init(); }
     });
     let cfg = GlobalConfig::new(0x0308);
-    let builder = DummyParserBuilder::<MockPipe>::new(cfg);
+    let builder = DummyParserBuilder::new(cfg);
     let mut parser = builder.build().ok().expect("Failed to build DummyParser");
     let mut msg = LogMessage::new();
     let input = "The quick brown ...";
@@ -82,7 +81,7 @@ fn test_parser_proxy_can_be_deinitialized() {
         unsafe { syslog_ng_global_init(); }
     });
     let cfg = GlobalConfig::new(0x0308);
-    let mut proxy = ParserProxy::<DummyParserBuilder<LogParser>>::new(cfg);
+    let mut proxy = ParserProxy::<DummyParserBuilder>::new(cfg);
     proxy.set_option("foo".to_owned(), "bar".to_owned());
     proxy.init();
     proxy.deinit();
@@ -94,7 +93,7 @@ use _parser_plugin::{native_parser_proxy_set_option};
 
 fn assert_option_setting_result_is_propagated(key: &str, expected: bool) {
     let mut proxy =
-        ParserProxy::with_builder_and_parser(Some(DummyParserBuilder(PhantomData)), None);
+        ParserProxy::with_builder_and_parser(Some(DummyParserBuilder{}), None);
     let key = CString::new(key).unwrap();
     let value = CString::new("value").unwrap();
     let actual = native_parser_proxy_set_option(&mut proxy, key.as_ptr(), value.as_ptr());
